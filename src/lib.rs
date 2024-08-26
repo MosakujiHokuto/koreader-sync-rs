6103ee3eaef5dd30c2e58478a8d80ba7807743d5
Youkou Tenhouin <youkou@tenhou.in>
2024-08-26 20:13:40 +0800

    feat: add management API for users

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use argon2::{
     Argon2,
 };
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
     http::{HeaderMap, StatusCode},
     routing::{get, post, put},
     Json, Router,
@@ -71,6 +71,8 @@ fn router(env: Env) -> Router {
         .route("/users/auth", get(auth_user))
         .route("/syncs/progress", put(update_progress))
         .route("/syncs/progress/:document", get(get_progress))
+        .route("/manage/users", get(mgm_get_users))
+        .route("/manage/users", post(mgm_create_user))
         .with_state(env)
 }
 
@@ -99,7 +101,7 @@ fn verify_password(pwd: &str, hash: &str) -> bool {
 
 async fn authenticate(d1: &D1Database, user: &str, pwd: &str) -> Result<Option<u32>> {
     let statement = d1.prepare("SELECT * FROM users WHERE name = ?1");
-    let query = statement.bind(&[user.trim().to_lowercase().into()])?;
+    let query = statement.bind(&[user.into()])?;
     if let Some(user) = query.first::<DBUser>(None).await? {
         if verify_password(pwd, &user.password) {
             Ok(Some(user.id))
@@ -115,16 +117,42 @@ async fn auth_header(d1: &D1Database, headers: &HeaderMap) -> Result<Option<u32>
     let user = headers.get("x-auth-user");
     let pwd = headers.get("x-auth-key");
     if let (Some(user), Some(pwd)) = (user, pwd) {
-        let user = str::from_utf8(user.as_bytes())?;
+        let user = normalize_username(str::from_utf8(user.as_bytes())?);
         let pwd = str::from_utf8(pwd.as_bytes())?;
-        authenticate(d1, user, pwd).await
+        authenticate(d1, user.as_str(), pwd).await
     } else {
         Ok(None)
     }
 }
 
+async fn auth_management(env: &Env, headers: &HeaderMap) -> Result<bool> {
+    let Some(header_secret) = headers.get("x-auth-mgm-token") else {
+        return Ok(false);
+    };
+    let Ok(header_secret) = str::from_utf8(header_secret.as_bytes()) else {
+        return Ok(false);
+    };
+    let secret = env.secret("MGM_TOKEN")?.to_string();
+    Ok(secret.len() > 0 && secret == header_secret)
+}
+
+async fn insert_user(d1: &D1Database, username: &str, password: &str) -> Result<Option<u32>> {
+    let password =
+        hash_password(password).map_err(|err| worker::Error::RustError(err.to_string()))?;
+    let statement = d1.prepare(concat!(
+        "INSERT INTO users (name, password) VALUES(?1, ?2) ",
+        "ON CONFLICT DO NOTHING RETURNING id"
+    ));
+    let query = statement.bind(&[username.into(), password.into()])?;
+    query.first::<u32>(Some("id")).await
+}
+
 fn d1_from_env(env: Env) -> Result<D1Database> {
     env.d1("DB_PROD_KOSYNC")
+}
+
+fn normalize_username(username: &str) -> String {
+    username.trim().to_lowercase()
 }
 
 pub async fn healthcheck() -> Json<Value> {
@@ -149,18 +177,9 @@ pub async fn create_user(
         let Ok(d1) = d1_from_env(env) else {
             return Ok(Error::NoDatabase.into_response());
         };
-        let statement = d1.prepare(concat!(
-            "INSERT INTO users (name, password) VALUES(?1, ?2) ",
-            "ON CONFLICT DO NOTHING RETURNING id"
-        ));
-        let username = payload.username.trim().to_lowercase();
-        let hashed_password = hash_password(&payload.password);
-        let Ok(password) = hashed_password else {
-            let err = hashed_password.unwrap_err();
-            return Ok(Error::Internal.with_message(err.to_string()));
-        };
-        let query = statement.bind(&[username.as_str().into(), password.into()])?;
-        if let Some(_id) = query.first::<u32>(Some("id")).await? {
+        let username = normalize_username(payload.username.as_str());
+        let result = insert_user(&d1, username.as_str(), payload.password.as_str()).await?;
+        if let Some(_id) = result {
             Ok((StatusCode::CREATED, Json(json!({ "username": username }))))
         } else {
             Ok(Error::UserExists.into_response())
@@ -329,4 +348,122 @@ pub async fn get_progress(
     do_get_progress(env, document, headers)
         .await
         .unwrap_or(Error::Internal.into_response())
+}
+
+fn default_offset() -> u32 {
+    0
+}
+
+fn default_limit() -> u32 {
+    50
+}
+
+#[derive(Deserialize)]
+pub struct PaginationQueryParams {
+    #[serde(default = "default_offset")]
+    offset: u32,
+    #[serde(default = "default_limit")]
+    limit: u32,
+}
+
+#[derive(Serialize)]
+struct MgmGetUserResponseEntry {
+    id: u32,
+    username: String,
+}
+
+impl From<DBUser> for MgmGetUserResponseEntry {
+    fn from(user: DBUser) -> MgmGetUserResponseEntry {
+        MgmGetUserResponseEntry {
+            id: user.id,
+            username: user.name,
+        }
+    }
+}
+
+pub async fn mgm_get_users(
+    State(env): State<Env>,
+    pagination: Query<PaginationQueryParams>,
+    headers: HeaderMap,
+) -> (StatusCode, Json<Value>) {
+    #[worker::send]
+    async fn do_mgm_get_users(
+        env: Env,
+        pagination: PaginationQueryParams,
+        headers: HeaderMap,
+    ) -> Result<(StatusCode, Json<Value>)> {
+        if !auth_management(&env, &headers).await? {
+            return Ok((
+                StatusCode::UNAUTHORIZED,
+                Json(json!({"error": "unauthorized"})),
+            ));
+        }
+        let d1 = d1_from_env(env)?;
+        let statement = d1.prepare("SELECT * FROM users LIMIT ?1 OFFSET ?2");
+        let query = statement
+            .bind(&[pagination.limit.into(), pagination.offset.into()])?
+            .all()
+            .await?;
+        let results: Vec<MgmGetUserResponseEntry> = query
+            .results::<DBUser>()?
+            .into_iter()
+            .map(|user| MgmGetUserResponseEntry::from(user))
+            .collect();
+        let value = serde_json::to_value(results)?;
+        return Ok((StatusCode::OK, Json(value)));
+    }
+    let ret = do_mgm_get_users(env, pagination.0, headers).await;
+    ret.unwrap_or_else(|err| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"error": err.to_string()})),
+        )
+    })
+}
+
+#[derive(Deserialize)]
+pub struct MgmCreateUserPayload {
+    username: String,
+    password: String,
+}
+
+pub async fn mgm_create_user(
+    State(env): State<Env>,
+    headers: HeaderMap,
+    Json(payload): Json<MgmCreateUserPayload>,
+) -> (StatusCode, Json<Value>) {
+    #[worker::send]
+    async fn do_mgm_create_user(
+        env: Env,
+        headers: HeaderMap,
+        payload: MgmCreateUserPayload,
+    ) -> Result<(StatusCode, Json<Value>)> {
+        if !auth_management(&env, &headers).await? {
+            return Ok((
+                StatusCode::UNAUTHORIZED,
+                Json(json!({"error": "unauthorized"})),
+            ));
+        }
+        let d1 = d1_from_env(env)?;
+        let username = normalize_username(payload.username.as_str());
+        let result = insert_user(&d1, username.as_str(), &payload.password).await?;
+        match result {
+            Some(id) => Ok((
+                StatusCode::CREATED,
+                Json(json!({ "id": id, "username": username})),
+            )),
+            None => Ok((
+                StatusCode::CONFLICT,
+                Json(json!({"error": "user already exists"})),
+            )),
+        }
+    }
+    do_mgm_create_user(env, headers, payload)
+        .await
+        .unwrap_or_else(|err| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"error": err.to_string()})),
+            )
+        })
 }
