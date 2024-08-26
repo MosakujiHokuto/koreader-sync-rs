1fe4c893309638447261978d3aa669ae1c68f0f5
Youkou Tenhouin <youkou@tenhou.in>
2024-08-26 14:19:24 +0800

    feat: progress sync

--- schema.sql
+++ schema.sql
@@ -4,3 +4,17 @@ CREATE TABLE users (
        name TEXT UNIQUE,
        password TEXT
 );
+
+DROP TABLE IF EXISTS progress;
+CREATE TABLE progress (
+       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+       document TEXT,
+       user_id INTEGER,
+       device_id TEXT,
+       device TEXT,
+       progress TEXT,
+       percentage REAL,
+       timestamp REAL,
+       FOREIGN KEY(user_id) REFERENCES users(id),
+       UNIQUE(document, user_id)
+);
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use axum::{
-    extract::State,
+    extract::{Path, State},
     http::{HeaderMap, StatusCode},
-    routing::{get, post},
+    routing::{get, post, put},
     Json, Router,
 };
+use chrono;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::str;
@@ -17,22 +18,44 @@ struct DBUser {
     password: String,
 }
 
+#[derive(Deserialize, Serialize)]
+struct DBProgress {
+    id: u32,
+    document: String,
+    user_id: u32,
+    device_id: String,
+    device: String,
+    progress: String,
+    percentage: f64,
+    timestamp: f64,
+}
+
 enum Error {
     NoDatabase,
     Internal,
     Unauthorized,
-    UserExists
+    UserExists,
 }
 
 impl Error {
-    fn into_response(&self) -> (StatusCode, Json<Value>) {
-        let code = match self {
+    fn code(&self) -> u32 {
+        match self {
             Self::NoDatabase => 1000,
             Self::Internal => 2000,
             Self::Unauthorized => 2001,
             Self::UserExists => 2002,
-        };
-        (StatusCode::BAD_REQUEST, Json(json!({"code": code})))
+        }
+    }
+
+    fn into_response(&self) -> (StatusCode, Json<Value>) {
+        (StatusCode::BAD_REQUEST, Json(json!({"code": self.code()})))
+    }
+
+    fn with_message(&self, message: String) -> (StatusCode, Json<Value>) {
+        (
+            StatusCode::BAD_REQUEST,
+            Json(json!({"code": self.code(), "message": message})),
+        )
     }
 }
 
@@ -41,6 +64,8 @@ fn router(env: Env) -> Router {
         .route("/healthcheck", get(healthcheck))
         .route("/users/create", post(create_user))
         .route("/users/auth", get(auth_user))
+        .route("/syncs/progress", put(update_progress))
+        .route("/syncs/progress/:document", get(get_progress))
         .with_state(env)
 }
 
@@ -54,17 +79,21 @@ async fn fetch(
     Ok(router(env).call(req).await?)
 }
 
-async fn authenticate(d1: &D1Database, user: &str, pwd: &str) -> Result<bool> {
-    let statement = d1.prepare("SELECT password FROM users WHERE name = ?1");
+async fn authenticate(d1: &D1Database, user: &str, pwd: &str) -> Result<Option<u32>> {
+    let statement = d1.prepare("SELECT * FROM users WHERE name = ?1");
     let query = statement.bind(&[user.trim().to_lowercase().into()])?;
-    if let Some(password) = query.first::<String>(Some("password")).await? {
-        Ok(password == pwd)
+    if let Some(user) = query.first::<DBUser>(None).await? {
+        if user.password == pwd {
+            Ok(Some(user.id))
+        } else {
+            Ok(None)
+        }
     } else {
-        Ok(false)
+        Ok(None)
     }
 }
 
-async fn auth_header(d1: &D1Database, headers: &HeaderMap) -> Result<bool> {
+async fn auth_header(d1: &D1Database, headers: &HeaderMap) -> Result<Option<u32>> {
     let user = headers.get("x-auth-user");
     let pwd = headers.get("x-auth-key");
     if let (Some(user), Some(pwd)) = (user, pwd) {
@@ -72,7 +101,7 @@ async fn auth_header(d1: &D1Database, headers: &HeaderMap) -> Result<bool> {
         let pwd = str::from_utf8(pwd.as_bytes())?;
         authenticate(d1, user, pwd).await
     } else {
-        Ok(false)
+        Ok(None)
     }
 }
 
@@ -125,13 +154,156 @@ pub async fn auth_user(State(env): State<Env>, headers: HeaderMap) -> (StatusCod
         let Ok(d1) = d1_from_env(env) else {
             return Ok(Error::NoDatabase.into_response());
         };
-        if auth_header(&d1, &headers).await? {
+        if let Some(_) = auth_header(&d1, &headers).await? {
             Ok((StatusCode::OK, Json(json!({ "authorized": "OK" }))))
         } else {
             Ok(Error::Unauthorized.into_response())
         }
     }
     do_auth_user(env, headers)
+        .await
+        .unwrap_or(Error::Internal.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct UpdateProgressPayload {
+    document: String,
+    percentage: f64,
+    progress: String,
+    device: String,
+    device_id: String,
+}
+
+#[derive(Serialize)]
+struct UpdateProgressResponse {
+    document: String,
+    timestamp: f64,
+}
+
+pub async fn update_progress(
+    State(env): State<Env>,
+    header: HeaderMap,
+    Json(payload): Json<UpdateProgressPayload>,
+) -> (StatusCode, Json<Value>) {
+    #[worker::send]
+    async fn do_update_progress(
+        env: Env,
+        header: HeaderMap,
+        payload: UpdateProgressPayload,
+    ) -> Result<(StatusCode, Json<Value>)> {
+        let Ok(d1) = d1_from_env(env) else {
+            return Ok(Error::NoDatabase.into_response());
+        };
+
+        let Some(user_id) = auth_header(&d1, &header).await? else {
+            return Ok(Error::Unauthorized.into_response());
+        };
+
+        let timestamp = chrono::Utc::now().timestamp_millis() as f64;
+        let timestamp = timestamp / 1000.0;
+
+        let statement = d1.prepare(concat!(
+            "INSERT INTO progress ",
+            "(document, user_id, device_id, device, progress, percentage, timestamp) ",
+            "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7) ",
+            "ON CONFLICT DO UPDATE SET ",
+            "device_id = ?3, ",
+            "device = ?4, ",
+            "progress = ?5, ",
+            "percentage = ?6, ",
+            "timestamp = ?7"
+        ));
+
+        let query = statement.bind(&[
+            payload.document.as_str().into(),
+            user_id.into(),
+            payload.device_id.into(),
+            payload.device.into(),
+            payload.progress.into(),
+            payload.percentage.into(),
+            timestamp.into(),
+        ])?;
+
+        let result = query.run().await?;
+
+        if result.success() {
+            Ok((
+                StatusCode::OK,
+                Json(
+                    serde_json::to_value(UpdateProgressResponse {
+                        document: payload.document,
+                        timestamp,
+                    })
+                    .unwrap(),
+                ),
+            ))
+        } else {
+            Ok(Error::Internal.into_response())
+        }
+    }
+    do_update_progress(env, header, payload)
+        .await
+        .unwrap_or_else(|err| Error::Internal.with_message(err.to_string()))
+}
+
+#[derive(Serialize)]
+struct GetProgressBody {
+    document: String,
+    percentage: f64,
+    progress: String,
+    device: String,
+    device_id: String,
+    timestamp: f64,
+}
+
+impl From<DBProgress> for GetProgressBody {
+    fn from(prog: DBProgress) -> Self {
+        Self {
+            document: prog.document,
+            percentage: prog.percentage,
+            progress: prog.progress,
+            device: prog.device,
+            device_id: prog.device_id,
+            timestamp: prog.timestamp,
+        }
+    }
+}
+
+pub async fn get_progress(
+    State(env): State<Env>,
+    Path(document): Path<String>,
+    headers: HeaderMap,
+) -> (StatusCode, Json<Value>) {
+    #[worker::send]
+    async fn do_get_progress(
+        env: Env,
+        document: String,
+        headers: HeaderMap,
+    ) -> Result<(StatusCode, Json<Value>)> {
+        let Ok(d1) = d1_from_env(env) else {
+            return Ok(Error::NoDatabase.into_response());
+        };
+
+        let Some(user_id) = auth_header(&d1, &headers).await? else {
+            return Ok(Error::Unauthorized.into_response());
+        };
+
+        let statement = d1.prepare(concat!(
+            "SELECT * FROM progress ",
+            "WHERE document = ?1 AND user_id = ?2"
+        ));
+        let query = statement.bind(&[document.into(), user_id.into()])?;
+
+        if let Some(result) = query.first::<DBProgress>(None).await? {
+            Ok((
+                StatusCode::OK,
+                Json(serde_json::to_value(GetProgressBody::from(result)).unwrap()),
+            ))
+        } else {
+            Ok((StatusCode::OK, Json(json!({}))))
+        }
+    }
+    do_get_progress(env, document, headers)
         .await
         .unwrap_or(Error::Internal.into_response())
 }
